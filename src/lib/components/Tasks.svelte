1df2a2fc44d14c4cdbcbdb9cf945006ff7b5a0e7
Stfnov <kovacsstefy46@gmail.com>
2024-05-05 12:24:33 +0300

    Changed complete icon.

--- src/lib/components/Tasks.svelte
+++ src/lib/components/Tasks.svelte
@@ -1,85 +1,82 @@
 <!-- TasksList.svelte -->
 <script>
-  // @ts-nocheck
-  import { Icon } from "svelte-icons-pack";
-  import { CiEdit } from "svelte-icons-pack/ci";
-  import { IoCheckmarkCircleOutline } from "svelte-icons-pack/io";
-  import { fetchTasksForUser } from "$lib/stores/database";
-  import { auth } from "$lib/firebase";
-  import EditTaskModal from "$lib/components/EditTaskModal.svelte";
-  import CompleteButton from "$lib/components/CompleteButton.svelte";
-  import {
-    selectedTaskStore,
-    selectedTaskStoreSecond,
-    tasksStore,
-  } from "$lib/stores/tasksStore"; // Import the selectedTaskStore
-  import { onMount } from "svelte";
+	// @ts-nocheck
+	import { Icon } from 'svelte-icons-pack';
+	import { CiEdit } from 'svelte-icons-pack/ci';
+	import { IoCheckmarkCircleOutline } from 'svelte-icons-pack/io';
+	import { SiTicktick } from "svelte-icons-pack/si";
+	import { fetchTasksForUser } from '$lib/stores/database';
+	import { auth } from '$lib/firebase';
+	import EditTaskModal from '$lib/components/EditTaskModal.svelte';
+	import CompleteButton from '$lib/components/CompleteButton.svelte';
+	import { selectedTaskStore, selectedTaskStoreSecond, tasksStore } from '$lib/stores/tasksStore'; // Import the selectedTaskStore
+	import { onMount } from 'svelte';
 
-  /**
-   * @type {any[]}
-   */
-  let tasks = [];
+	/**
+	 * @type {any[]}
+	 */
+	let tasks = [];
 
-  onMount(async () => {
-    const user = auth.currentUser;
-    if (user) {
-      try {
-        // Fetch tasks for the logged-in user
-        tasks = await fetchTasksForUser();
-        tasksStore.set(tasks); // Update tasksStore with fetched tasks
-        console.log("Reactive tasks:", $tasksStore); // Log tasksStore for debugging
-      } catch (error) {
-        console.error("Error fetching tasks:", error);
-      }
-    }
-  });
+	onMount(async () => {
+		const user = auth.currentUser;
+		if (user) {
+			try {
+				// Fetch tasks for the logged-in user
+				tasks = await fetchTasksForUser();
+				tasksStore.set(tasks); // Update tasksStore with fetched tasks
+				console.log('Reactive tasks:', $tasksStore); // Log tasksStore for debugging
+			} catch (error) {
+				console.error('Error fetching tasks:', error);
+			}
+		}
+	});
 
-  // Listen for changes in selectedTaskStoreSecond
-  // $: {
-  //   const completedTask = $selectedTaskStoreSecond;
-  //   if (completedTask) {
-  //     // Remove the completed task from the tasks list
-  //     tasks = tasks.filter((task) => task.id !== completedTask.id);
-  //   }
-  // }
+	// Function to open the edit task modal
+	function openEditModal(task) {
+		selectedTaskStore.set(task);
+	}
 
-  // Function to open the edit task modal
-  function openEditModal(task) {
-    selectedTaskStore.set(task);
-  }
-
-  // Function to open the complete task modal
-  function openCompleteModal(task) {
-    selectedTaskStoreSecond.set(task);
-  }
+	// Function to open the complete task modal
+	function openCompleteModal(task) {
+		selectedTaskStoreSecond.set(task);
+	}
 </script>
 
 <div class="tasks-container">
-  <!-- Render the tasks data -->
-  {#each $tasksStore.filter((task) => !task.completed) as task}
-    <div class="task">
-      <div class="task-details">
-        <p class="title">{task.title}</p>
-        <p class="description">{task.description}</p>
-      </div>
-      <div class="task-buttons">
-        <button on:click={() => openEditModal(task)} class="edit"
-          ><Icon src={CiEdit} color="#fff" size="26px" className="edit-icon" /></button
-        >
-        <!-- svelte-ignore missing-declaration -->
-        <button on:click={() => openCompleteModal(task)} class="complete"
-          ><Icon src={IoCheckmarkCircleOutline} color="#fff" stroke="#fff" size="26px" className="complete-icon"
-          /></button
-        >
-      </div>
-    </div>
-  {/each}
-  <EditTaskModal {selectedTaskStore} />
-  <CompleteButton {selectedTaskStoreSecond} />
+	<!-- Render the tasks data -->
+	{#each $tasksStore.filter((task) => !task.completed) as task}
+		<div class="task">
+			<div class="task-details">
+				<p class="title">{task.title}</p>
+				<p class="description">{task.description}</p>
+			</div>
+			<div class="task-buttons">
+				<button on:click={() => openEditModal(task)} class="edit"
+					><Icon src={CiEdit} color="#fff" size="26px" className="edit-icon" /></button
+				>
+				<!-- svelte-ignore missing-declaration -->
+				<button on:click={() => openCompleteModal(task)} class="complete"
+					><Icon
+						src={SiTicktick}
+						color="#fff"
+						stroke="#fff"
+						size="21px"
+						className="complete-icon"
+					/></button
+				>
+			</div>
+		</div>
+	{/each}
+	<EditTaskModal {selectedTaskStore} />
+	<CompleteButton {selectedTaskStoreSecond} />
 </div>
 
 <style lang="scss">
-  @media screen and (max-width: 768px) {
+	.complete-icon {
+		color: #fff;
+	}
+
+	@media screen and (max-width: 768px) {
 		.tasks-container {
 			padding: 7.5px;
 			margin-top: 15px;
@@ -200,7 +197,7 @@
 		}
 	}
 
-@media screen and (min-width: 768px) {
+	@media screen and (min-width: 768px) {
 		.complete-icon {
 			// complete icon
 			path {
